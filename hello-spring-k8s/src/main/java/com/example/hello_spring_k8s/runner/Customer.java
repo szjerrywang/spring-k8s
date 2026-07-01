3fda7e20f80b2625175def315ed6abd780eae7d2
Jerry Wang <szjerrywang@163.com>
2026-07-01 17:00:15 +0800

    initial

---
+++ hello-spring-k8s/src/main/java/com/example/hello_spring_k8s/runner/Customer.java
@@ -0,0 +1,11 @@
+package com.example.hello_spring_k8s.runner;
+
+public record Customer(long id, String firstName, String lastName) {
+
+	@Override
+	public String toString() {
+		return String.format(
+				"Customer[id=%d, firstName='%s', lastName='%s']",
+				id, firstName, lastName);
+	}
+}
---
+++ hello-spring-k8s/src/main/java/com/example/hello_spring_k8s/runner/DbChecker.java
@@ -0,0 +1,50 @@
+package com.example.hello_spring_k8s.runner;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.core.annotation.Order;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+@Component
+@Order(1)
+public class DbChecker implements CommandLineRunner {
+    private static final Logger log = LoggerFactory.getLogger(DbChecker.class);
+
+    private final JdbcTemplate jdbcTemplate;
+
+    public DbChecker(JdbcTemplate jdbcTemplate) {
+        this.jdbcTemplate = jdbcTemplate;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        log.info("Creating tables");
+
+        jdbcTemplate.execute("DROP TABLE IF EXISTS customers");
+        jdbcTemplate.execute("CREATE TABLE customers(" +
+                "id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY, first_name VARCHAR(255), last_name VARCHAR(255))");
+
+        // Split up the array of whole names into an array of first/last names
+        List<Object[]> splitUpNames = Stream.of("John Woo", "Jeff Dean", "Josh Bloch", "Josh Long")
+                .map(name -> name.split(" "))
+                .collect(Collectors.toList());
+
+        // Use a Java 8 stream to print out each tuple of the list
+        splitUpNames.forEach(name -> log.info("Inserting customer record for {} {}", name[0], name[1]));
+
+        // Use JdbcTemplate's batchUpdate operation to bulk load data
+        jdbcTemplate.batchUpdate("INSERT INTO customers(first_name, last_name) VALUES (?,?)", splitUpNames);
+
+        log.info("Querying for customer records where first_name = 'Josh':");
+        jdbcTemplate.query(
+                        "SELECT id, first_name, last_name FROM customers WHERE first_name = ?",
+                        (rs, rowNum) -> new Customer(rs.getLong("id"), rs.getString("first_name"), rs.getString("last_name")), "Josh")
+                .forEach(customer -> log.info(customer.toString()));
+    }
+}
